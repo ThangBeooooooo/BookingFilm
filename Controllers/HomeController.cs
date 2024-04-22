d29012c08299e1109c7257a399b206b0fe64f8fa
Nguyễn Thái Hoàng <2151010115hoang@ou.edu.vn>
2024-04-22 23:13:11 +0700

    22/4 update home

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,10 +8,27 @@ namespace BookingFilm.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
-        public ActionResult Index()
-        {
-            return View();
-        }
-    }
+		private readonly BookingFilmTicketsEntities1 _context;
+		public HomeController()
+		{
+			_context = new BookingFilmTicketsEntities1();
+		}
+		// GET: Home
+		public ActionResult Index()
+		{
+			return View();
+		}
+		public ActionResult GetMovies()
+		{
+			var cards = _context.Phims.Select(c => new
+			{
+				background = c.HinhPhim,
+				display_background = c.HinhPhim,
+				title = c.TenPhim,
+				description = c.MoTa
+			}).ToList();
+
+			return Json(cards, JsonRequestBehavior.AllowGet);
+		}
+	}
 }
--- JS/app.js
+++ JS/app.js
@@ -1,84 +1,32 @@
 ﻿let num_cards_GLOBAL = 5;
 
-let cards = [
-    {
-        background:
-            "//external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fmoviefail.com%2Fwp-content%2Fuploads%2F2014%2F11%2Finterstellar1.jpeg&f=1&nofb=1",
-        display_background:
-            "//external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fhdqwalls.com%2Fdownload%2Finterstellar-gargantua-u4-1920x1080.jpg&f=1&nofb=1",
-        title: "Interstellar",
-        description:
-            "Watch this incredible film made by some incredible people!"
-    },
-    {
-        background: "https://images.alphacoders.com/586/thumb-1920-586902.jpg",
-        display_background:
-            "//external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages2.alphacoders.com%2F845%2F84502.jpg&f=1&nofb=1",
-        title: "Inception",
-        description:
-            "Watch this incredible film made by some incredible people!"
-    },
-    {
-        background:
-            "//external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.wallpapersafari.com%2F41%2F1%2Fhj197K.jpg&f=1&nofb=1",
-        display_background:
-            "//external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages7.alphacoders.com%2F990%2F990610.jpg&f=1&nofb=1",
-        title: "Avengers: Endgame",
-        description:
-            "Watch this incredible film made by some incredible people!"
-    },
-    {
-        background:
-            "//external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages.wallpapersden.com%2Fimage%2Fdownload%2Fjoker-2019-movie_66632_1920x1080.jpg&f=1&nofb=1",
-        display_background:
-            "//external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages.wallpapersden.com%2Fimage%2Fdownload%2Fjoker-stair-dance_68124_1920x1080.jpg&f=1&nofb=1",
-        title: "Joker",
-        description:
-            "Watch this incredible film made by some incredible people!"
-    },
-    {
-        background:
-            "//external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.U2iFUz9mivZeH-h48O5wtwHaEK%26pid%3DApi&f=1",
-        title: "1917",
-        description:
-            "Watch this incredible film made by some incredible people!"
-    },
-    {
-        background:
-            "//external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpapershome.com%2Fimages%2Fpages%2Fpic_h%2F22685.jpg&f=1&nofb=1",
-        title: "TENET",
-        description:
-            "Watch this incredible film made by some incredible people!"
-    },
-    {
-        background:
-            "//external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.hdwallpapers.in%2Fdownload%2Fskyfall_2012_movie-1920x1080.jpg&f=1&nofb=1",
-        title: "Skyfall",
-        description:
-            "Watch this incredible film made by some incredible people!"
-    },
-    {
-        background:
-            "//external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fgetwallpapers.com%2Fwallpaper%2Ffull%2F9%2F6%2F8%2F126147.jpg&f=1&nofb=1",
-        title: "Star Wars: A New Hope",
-        description:
-            "Watch this incredible film made by some incredible people!"
-    },
-    {
-        background:
-            "//external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimage.tmdb.org%2Ft%2Fp%2Foriginal%2FrPpxrz8o0svAPCLucjsEdMXoDfX.jpg&f=1&nofb=1",
-        title: "Venom",
-        description:
-            "Watch this incredible film made by some incredible people!"
-    },
-    {
-        background:
-            "//external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpapertag.com%2Fwallpaper%2Ffull%2F2%2F6%2F8%2F123320-lord-of-the-rings-background-1920x1080-picture.jpg&f=1&nofb=1",
-        title: "Lord of the Rings",
-        description:
-            "Watch this incredible film made by some incredible people!"
+let cards = [];
+
+async function fetchData() {
+    try {
+        const response = await fetch('/Home/GetMovies');
+        const data = await response.json()
+
+        return data.map(item => ({
+            background: item.background,
+            display_background: item.background,
+            title: item.title,
+            description: item.description
+        }))
+    } catch (error) {
+        console.error('Error:', error)
+        return []; // Trả về một mảng rỗng nếu có lỗi
     }
-];
+}
+
+async function loadPage() {
+    const phims = await fetchData()
+    cards = phims
+    console.log(cards.length)
+    AddCarouselButtons(); CheckSizeAttributes(); AddCards(); MakeJumbotron(); CheckCards(); ResizeHeader();
+}
+
+window.addEventListener('load', loadPage); // Gọi hàm loadPage khi trang được tải xong
 
 let cast = [
     {
