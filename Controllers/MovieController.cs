8e3138b807d45ce52f1a78ecb2818ecfdf163702
Nguyễn Thái Hoàng <2151010115hoang@ou.edu.vn>
2024-04-24 02:16:09 +0700

    update Movie but not done

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,83 @@
+﻿using CloudinaryDotNet.Actions;
+using CloudinaryDotNet;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookingFilm.Controllers
+{
+    public class MovieController : Controller
+    {
+		// GET: Movie
+		private readonly BookingFilmTicketsEntities1 _context;
+
+		public MovieController()
+		{
+			_context = new BookingFilmTicketsEntities1();
+		}
+
+		public ActionResult Index()
+		{
+			var khachHangs = _context.Phims.ToList(); // Truy vấn dữ liệu từ database
+			return View(khachHangs); // Truyền dữ liệu sang View
+		}
+
+		public ActionResult Create()
+		{
+			return View();
+		}
+
+		[Obsolete]
+		public string UrlImageAfterUpload(HttpPostedFileBase HinhAnh)
+		{
+			var account = new Account(
+						"dzamheemx",  // Cloud name
+						"279156174534789",  // API key
+						"KFQQWMyliAcvwK7vYvX__qEYstM"   // API secret
+					);
+
+			var cloudinary = new Cloudinary(account);
+
+			var uploadParams = new ImageUploadParams()
+			{
+				File = new FileDescription(Path.GetFileName(HinhAnh.FileName), HinhAnh.InputStream),
+				UploadPreset = "ml_default"  // Upload preset name
+			};
+
+			var uploadResult = cloudinary.Upload(uploadParams);
+			return uploadResult.SecureUri.AbsoluteUri;
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Create([Bind(Include = "MaPhim,TenPhim,TheLoai,ThoiLuong,DaoDien,NamSanXuat,HinhPhim,MoTa")] BookingFilm.Phim phim, HttpPostedFileBase HinhP)
+		{
+			if (ModelState.IsValid)
+			{
+				phim.HinhPhim = UrlImageAfterUpload(HinhP);
+				_context.Phims.Add(phim);
+				_context.SaveChanges();
+				return RedirectToAction("Index");
+			}
+
+			return View(phim);
+		}
+
+
+		public ActionResult Delete(int id)
+		{
+			var phim = _context.Phims.Find(id);
+			if (phim == null)
+			{
+				return HttpNotFound();
+			}
+
+			_context.Phims.Remove(phim);
+			_context.SaveChanges();
+			return RedirectToAction("Index");
+		}
+	}
+}
--- DoAn.cs
+++ DoAn.cs
@@ -28,5 +28,4 @@ namespace BookingFilm
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<DatVe> DatVes { get; set; }
     }
-
 }
